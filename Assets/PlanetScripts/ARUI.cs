5ba0f31ac4cf6a7c3b8403edddaa0f6c31a9e6b3
excaliber90 <thapaprashant06@gmail.com>
2025-07-08 00:12:02 +0545

    UI changes on Main menu

--- Assets/PlanetScripts/ARUI.cs
+++ Assets/PlanetScripts/ARUI.cs
@@ -39,7 +39,7 @@ private Dictionary<string, QuizData> quizDictionary = new Dictionary<string, Qui
         /*if (GetbackButton != null) {
             GetbackButton.gameObject.SetActive(false); //Hide Go back button initially
         }*/
-    quizDictionary.Add("earth", new QuizData {
+    quizDictionary.Add("Earth", new QuizData {
     question = "Which planet supports life?",
     options = new string[] { "Mars", "Venus", "Earth" },
     correctAnswerIndex = 2
--- Assets/PlanetScripts/Gobackbutton.cs
+++ Assets/PlanetScripts/Gobackbutton.cs
@@ -4,7 +4,6 @@ using UnityEngine.SceneManagement;
 public class Gobackbutton : MonoBehaviour
 {
     // let me do back to the main menu
-
     public string sceneToLoad = "Main_Menu"; 
 
     public void GoBack()
